650bf606cf446bb355899cd26d59380390c5666b
firebladed <34522909+firebladed@users.noreply.github.com>
2020-10-01 16:54:28 +0100

    Update opencv_mlx90640.py

--- opencv_mlx90640/opencv_mlx90640.py
+++ opencv_mlx90640/opencv_mlx90640.py
@@ -10,6 +10,6 @@ class mlx90640:
 
   def check_device(devpath):
 
-    if "/sys/class/video4linux/".os.path.basename(self.devnum)."/device/name" != "mlx90640"
+    if "/sys/class/video4linux/"+os.path.basename(self.devnum)+"/device/name" != "mlx90640"
       raise Exception("Not mlx90640")
 
