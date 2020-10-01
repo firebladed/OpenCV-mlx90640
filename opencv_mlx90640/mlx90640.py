9a3ff8d60ca314e9c86de0ea969f482a8399f8c9
firebladed <34522909+firebladed@users.noreply.github.com>
2020-10-01 17:48:28 +0100

    Update mlx90640.py

--- opencv_mlx90640/mlx90640.py
+++ opencv_mlx90640/mlx90640.py
@@ -1,17 +1,28 @@
-#opencv-mlx90640.py
+#opencv_mlx90640.py
 #
+import os
+import cv2
+
 class mlx90640:
 
   devpath = ""
+  eeprom = None
 
   def __init__(self,devpath):
     self.devpath= devpath
     self.check_device()
 
-  def check_device(devpath):
+  def check_device(self):
 
-    with open("/sys/class/video4linux/"+os.path.basename(self.devnum)+"/device/name", 'r') as file:
+    with open("/sys/class/video4linux/"+os.path.basename(self.devpath)+"/device/name", 'r') as file:
       data = file.read()
-      if data != "mlx90640"
+      #print(data)
+      if data.strip() != "mlx90640":
         raise Exception("Not mlx90640")
 
+    with open("/sys/class/video4linux/"+os.path.basename(self.devpath)+"/device/mlx90640_nvram0/nvmem", mode='rb') as eeprom:
+      self.eeprom = eeprom.read()
+
+    print(self.eeprom)
+
+  def correct_img(self)
